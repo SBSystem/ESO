18b259aae2ce7170a160801cd3105ba9da10c9b2
Mariusz08 <nowygracz01@gmail.com>
2018-03-19 17:35:21 +0100

    Added Entities 	renamed:       src/Controller/admin/DashboardController.php -> src/Controller/Admin/DashboardController.php 	nowy plik:     src/Entity/Room.php 	nowy plik:     src/Entity/Subject.php 	nowy plik:     src/Entity/User.php 	nowy plik:     src/Migrations/Version20180319152148.php 	nowy plik:     src/Migrations/Version20180319155027.php 	nowy plik:     src/Repository/RoomRepository.php 	nowy plik:     src/Repository/SubjectRepository.php 	nowy plik:     src/Repository/UserRepository.php

---
+++ src/Entity/User.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $login;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $logged;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $role;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $code;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $surname;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $pesel;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getLogin(): ?string
+    {
+        return $this->login;
+    }
+
+    public function setLogin(string $login): self
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getLogged(): ?int
+    {
+        return $this->logged;
+    }
+
+    public function setLogged(int $logged): self
+    {
+        $this->logged = $logged;
+
+        return $this;
+    }
+
+    public function getRole(): ?string
+    {
+        return $this->role;
+    }
+
+    public function setRole(string $role): self
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    public function getCode(): ?int
+    {
+        return $this->code;
+    }
+
+    public function setCode(?int $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSurname(): ?string
+    {
+        return $this->surname;
+    }
+
+    public function setSurname(string $surname): self
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    public function getPesel(): ?int
+    {
+        return $this->pesel;
+    }
+
+    public function setPesel(?int $pesel): self
+    {
+        $this->pesel = $pesel;
+
+        return $this;
+    }
+}
