b3cd06961cdf9573a0793157f57d27695ef494a3
Mariusz08 <nowygracz01@gmail.com>
2018-03-22 11:59:02 +0100

    Update User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,13 +1,11 @@
 <?php
-
 namespace App\Entity;
-
 use Doctrine\ORM\Mapping as ORM;
-
+use Symfony\Component\Security\Core\User\UserInterface;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
-class User
+class User implements UserInterface, \Serializable
 {
     /**
      * @ORM\Id()
@@ -15,162 +13,126 @@ class User
      * @ORM\Column(type="integer")
      */
     private $id;
-
     /**
      * @ORM\Column(type="string", length=50)
      */
-    private $login;
-
+    private $username;
     /**
-     * @ORM\Column(type="string", length=30)
+     * @ORM\Column(type="string", length=50)
      */
     private $password;
-
     /**
      * @ORM\Column(type="string", length=50)
      */
     private $email;
-
     /**
-     * @ORM\Column(type="integer")
+     * @ORM\Column(type="boolean")
      */
     private $logged;
-
     /**
      * @ORM\Column(type="string", length=20)
      */
     private $role;
-
     /**
      * @ORM\Column(type="integer", nullable=true)
      */
     private $code;
-
     /**
      * @ORM\Column(type="string", length=20)
      */
     private $name;
-
     /**
      * @ORM\Column(type="string", length=30)
      */
     private $surname;
-
     /**
      * @ORM\Column(type="integer", nullable=true)
      */
     private $pesel;
-  
+	
     public function getId()
     {
         return $this->id;
     }
-
-    public function getLogin(): ?string
+    public function getUsername(): ?string
     {
-        return $this->login;
+        return $this->username;
     }
-
-    public function setLogin(string $login): self
+    public function setUsername(string $username): self
     {
-        $this->login = $login;
-
+        $this->username = $username;
         return $this;
     }
-
     public function getPassword(): ?string
     {
         return $this->password;
     }
-
     public function setPassword(string $password): self
     {
         $this->password = $password;
-
         return $this;
     }
-
     public function getEmail(): ?string
     {
         return $this->email;
     }
-
     public function setEmail(string $email): self
     {
         $this->email = $email;
-
         return $this;
     }
-
-    public function getLogged(): ?int
+    public function getLogged(): bool
     {
         return $this->logged;
     }
-
     public function setLogged(int $logged): self
     {
         $this->logged = $logged;
-
         return $this;
     }
-
-    public function getRole(): ?string
+    public function getRoles(): array
     {
         return $this->role;
     }
-
     public function setRole(string $role): self
     {
         $this->role = $role;
-
         return $this;
     }
-
     public function getCode(): ?int
     {
         return $this->code;
     }
-
     public function setCode(?int $code): self
     {
         $this->code = $code;
-
         return $this;
     }
-
     public function getName(): ?string
     {
         return $this->name;
     }
-
     public function setName(string $name): self
     {
         $this->name = $name;
-
         return $this;
     }
-
     public function getSurname(): ?string
     {
         return $this->surname;
     }
-
     public function setSurname(string $surname): self
     {
         $this->surname = $surname;
-
         return $this;
     }
-
     public function getPesel(): ?int
     {
         return $this->pesel;
     }
-
     public function setPesel(?int $pesel): self
     {
         $this->pesel = $pesel;
-
         return $this;
     }
 }
