35d9dceb8d530cf48e68e02797a446a38c4aded8
Mariusz08 <nowygracz01@gmail.com>
2018-03-22 11:59:18 +0100

    Update User.php

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,7 +1,10 @@
 <?php
+
 namespace App\Entity;
+
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
